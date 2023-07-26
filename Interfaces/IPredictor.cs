695bb17a4c9158aae9f6f7f9b49c997f877f28f3
Marek <marektobias@email.cz>
2023-07-26 23:09:13 +0200

    Added IPredictor for future model predictions

---
+++ Interfaces/IPredictor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Analysis;
+
+namespace RecommendationSystem.Interfaces
+{
+    internal interface IPredictor
+    {
+        void Predict(DataFrame dataFrame);
+    }
+}
