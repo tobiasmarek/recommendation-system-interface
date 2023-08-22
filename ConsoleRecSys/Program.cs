b2f6a353b553db49bcac7e2449502b1a5c73db72
Marek <marektobias@email.cz>
2023-08-23 00:01:58 +0200

    Added ConsoleAppProject

---
+++ ConsoleRecSys/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using RecommendationSystemInterface;
+
+namespace ConsoleRecSys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var session = new ConsoleSession();
+        }
+    }
+
+
+    class ConsoleSubjectSession : ConsoleSession
+    {
+        public void AddFavourite() // tady spis ne, nebo ne?
+        {
+
+        }
+    }
+}
