6fb794c7ec78498890a65f2331f6e1d10a278652
Marek <marektobias@email.cz>
2023-08-24 16:12:08 +0200

    Moved redundancies

--- RecommendationSystemInterface/Controller.cs
+++ RecommendationSystemInterface/Controller.cs
@@ -14,41 +14,6 @@ namespace RecommendationSystemInterface
         public Session Session { get; set; } // The Session it controls
 
         public abstract void TakeInput(); // NEBO MISTO TOHO JEN INTERFACE KTEREJ TakeInput IMPLEMENTUJE A PARSUJE ODKUDKOLIV
-
-        public void TakeCommand(string[] cmd) // CHCI MIT MOZNOST DEFINOVAT SVOJE CASES
-        {
-            switch (cmd[0]) // NEBO ZPRACOVAVAT VZDY V INTERFACU?
-            {
-            case "loadfromcsv":
-                if (cmd.Length == 2) {Session.LoadFromCsv(cmd[1]);}
-                else {Session.LoadFromCsv();}
-                break;
-            case "loadfromdbs":
-                Session.LoadFromDbs();
-                break;
-            case "selectapproach":
-                Session.SelectApproach();
-                break;
-            case "recommend":
-                Session.GetRecommendations();
-                break;
-            case "savesession":
-                Session.SaveSession(cmd[1]);
-                break;
-            case "loadsession":
-                Session? loadedSession = Session.LoadSession(cmd[1]);
-                if (loadedSession != null) { Session.Controller = this; } // TAKZE BYCH MEL UKLADAT JEN APPROACH, USER A DATA, ABYCH TOHLE NEMUSEL DELAT
-                    break; 
-            case "showsessions":
-                Session.ShowSessions();
-                break;
-            case "deletesession":
-                Session.DeleteSession(cmd[1]);
-                break;
-            default:
-                return;
-            }
-        }
     }
 
 
@@ -61,36 +26,7 @@ namespace RecommendationSystemInterface
     {
         public override void TakeInput()
         {
-            Console.WriteLine(@"            case ""loadfromcsv"":
-                Session.LoadFromCsv();
-                break;
-            case ""loadfromdbs"":
-                Session.LoadFromDbs();
-                break;
-            case ""selectapproach"":
-                Session.SelectApproach();
-                break;
-            case ""recommend"":
-                Session.GetRecommendations();
-                break;
-            case ""savesession"":
-                Session.SaveSession(cmd[1]);
-                break;
-            case ""loadsession"":
-                Session? loadedSession = Session.LoadSession(cmd[1]);
-                if (loadedSession != null) { Session.Controller = this; } // takze bych mel spis ukladat jen approach, user a data, abych nemusel tohle
-                break;
-            case ""showsessions"":
-                Session.ShowSessions();
-                break;
-            case ""deletesession"":
-                Session.DeleteSession(cmd[1]);");
-
-            string? line;
-            while ((line = Console.ReadLine()) != "")
-            {
-                TakeCommand(line.ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries));
-            }
+            throw new NotImplementedException();
         }
     }
 
