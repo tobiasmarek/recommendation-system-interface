e44bc4ebccc01177e2a60defd4cb4b84433a43f8
Marek <marektobias@email.cz>
2023-09-01 14:20:19 +0200

    Extended Viewer in the Console

--- ConsoleRecSys/ConsoleViewer.cs
+++ ConsoleRecSys/ConsoleViewer.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Text;
 using RecommendationSystemInterface;
 
 namespace ConsoleRecSys
 {
     class ConsoleViewer : Viewer
     {
+        private const string ErrorMsg = "Wrong command format - should be '";
+
         public override void ViewFile(string filePath)
         {
             string[] lines = ReadFirstKLines(filePath, 5);
@@ -18,6 +21,32 @@ namespace ConsoleRecSys
         public override void ViewString(string str)
         {
             Console.WriteLine(str);
+        }
+
+        public void ShowIndexedArray(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{i}: {array[i]}");
+            }
+        }
+
+        public void WrongCmdErrorMsg(string[] parameters)
+        {
+            if (parameters.Length == 0) { return; }
+
+            var sb = new StringBuilder();
+            sb.Append(ErrorMsg);
+            sb.Append(parameters[0]);
+
+            for (int i = 1; i < parameters.Length; i++)
+            {
+                sb.Append($" [{parameters[i]}]");
+            }
+
+            sb.Append('\'');
+
+            Console.WriteLine(sb.ToString());
         }
     }
 }
